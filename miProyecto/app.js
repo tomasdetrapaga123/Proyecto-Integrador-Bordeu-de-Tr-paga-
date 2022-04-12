e30aa92cbbd272d5d32efab715025feb9b3f8471
tomasdetrapaga123 <tdetrapaga@udesa.edu.ar>
2022-04-12 09:43:17 -0300

    refactor (routes): organizacion del ruteo

--- miProyecto/app.js
+++ miProyecto/app.js
@@ -5,8 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var productsRouter = require('./routes/products');
 
 var app = express();
 
@@ -21,9 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/product', productsRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- miProyecto/routes/index.js
+++ miProyecto/routes/index.js
@@ -1,20 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/indexControllers');
+const usersRouter = require('../routes/users')
+const productsRouter = require('../routes/products')
 
 /* GET home page. */
-router.get('/', controller.index)
-router.get('/index', controller.index)
-router.get('/register', controller.register)
-router.get('/login', controller.login)
-router.get('/search', controller.search)
+router.get('/', controller.index);
+router.get('/index', controller.index);
+router.get('/register', controller.register);
+router.get('/login', controller.login);
+router.get('/search', controller.search);
+router.use('/users', usersRouter);
+router.use('/products', productsRouter);
+
 
 module.exports = router;
-
-
-
-
-
-
-
-
