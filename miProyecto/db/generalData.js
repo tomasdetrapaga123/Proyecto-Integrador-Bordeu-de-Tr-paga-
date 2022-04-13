44d69ead3ff307da0a38f1347c98904aceae708c
tomasdetrapaga123 <tdetrapaga@udesa.edu.ar>
2022-04-13 14:38:09 -0300

    agrego mas comentarios de los productos a la base de datos

--- miProyecto/db/generalData.js
+++ miProyecto/db/generalData.js
@@ -10,48 +10,82 @@ const data = {
 
     comments: [ {
         id: 1,
-        userName: 'Tomás de Trápaga',
+        userName: 'Tomas De Trapaga',
+        userEmail: 'tdetrapaga@udesa.edu.ar',
         textoDelComentario: 'Muy linda la camiseta',
         imagenDelPerfil: '',
+        productId: 1,
     },
 
     {
         id: 2,
         userName: 'Marcos Bordeu',
+        userEmail: 'mbordeu@udesa.edu.ar',
         textoDelComentario: 'No me gusto mucho la camiseta',
         imagenDelPerfil: '',
+        productId: 1,
     },
 
     {
         id: 3,
-        userName: 'Miguel Guerrero',
+        userName: 'Miguel Guerreo',
+        userEmail: 'mguerrero@udesa.edu.ar',
         textoDelComentario: 'Estaba buscando una camiseta nueva y se nota que ya está usada',
         imagenDelPerfil: '',
+        productId: 1,
     },
 
     {
         id: 4,
         userName: 'Felipe Floria',
+        userEmail: 'floriaf@northland.edu.ar',
         textoDelComentario: 'Cual es el precio? Muy linda!',
         imagenDelPerfil: '',
+        productId: 2,
     },
 
     {
         id: 5,
         userName: 'Rafael Larreta',
+
+        userEmail: 'rafalarreta@northlands.edu.ar',
         textoDelComentario: '¿Tienen la camiseta titular actual del equipo?',
         imagenDelPerfil: '',
+        productId: 2,
     },
 
     {
         id: 6,
         userName: 'Franco Ferrero',
-        textoDelComentario: 'Me gusto mucho!',
+        userEmail: 'ferrerof@udesa.edu.ar',
+        textoDelComentario: 'Me gusto meti muchos goles con esta casaca!',
         imagenDelPerfil: '',
+        productId: 3,
     },
-
-
-
+    {
+        id: 7,
+        userName: 'Franco Ferrero',
+        userEmail: 'ferrerof@udesa.edu.ar',
+        textoDelComentario: 'Me quedo chica la remera!',
+        imagenDelPerfil: '',
+        productId: 3,
+    },
+    {
+        id: 8,
+        userName: 'Franco Ferrero',
+        userEmail: 'ferrerof@udesa.edu.ar',
+        textoDelComentario: 'Me encanto este producto!',
+        imagenDelPerfil: '',
+        productId: 4,
+    },
+    {
+        id: 9,
+        userName: 'Franco Ferrero',
+        userEmail: 'ferrerof@udesa.edu.ar',
+        textoDelComentario: 'Me encanto!',
+        imagenDelPerfil: '',
+        productId: 4,
+    },
     ],
 
     products: [ {
@@ -59,6 +93,7 @@ const data = {
         name: 'Camiseta Inter',
         description: 'Camiseta del Inter de Milan alternativa',
         imageName: 'imageMilan.jpg',
+        commentsCount: 3,
     },
 
     {
@@ -66,6 +101,7 @@ const data = {
         name: 'Camiseta Independiente',
         description: 'Camiseta de Independiente retro',
         imageName: 'imageIndependiente.jpg',
+        commentsCount: 2,
     },
 
     {
@@ -73,6 +109,7 @@ const data = {
         name: 'Camiseta Holanda',
         description: 'Camiseta de Holanda retro',
         imageName: 'imageHolanda.jpg',
+        commentsCount: 2,
     },
 
     {
@@ -80,6 +117,7 @@ const data = {
         name: 'Camiseta Barcelona',
         description: 'Camiseta del Barcelona alternativa',
         imageName: 'imageBarcelona.jpg',
+        commentsCount: 2,
     },
 
     {
@@ -87,6 +125,7 @@ const data = {
         name: 'Camiseta Manchester City',
         description: 'Camiseta del Manchester City titular',
         imageName: 'imageManCity.jpg',
+        commentsCount: 1,
     },
 
     {
@@ -94,6 +133,7 @@ const data = {
         name: 'Camiseta Real Madrid',
         description: 'Camiseta del Real Madrid',
         imageName: 'imageRealMadrid.jpg',
+        commentsCount: 0,
     },
 
     {
@@ -101,6 +141,7 @@ const data = {
         name: 'Camiseta Liverpool',
         description: 'Camiseta del Liverpool alternativa',
         imageName: 'imageLiverpool.jpg',
+        commentsCount: 1,
     },
 
     {
@@ -108,6 +149,7 @@ const data = {
         name: 'Camiseta Argentina',
         description: 'Camiseta de Argentina retro',
         imageName: 'imageArgentina.jpg',
+        commentsCount: 0,
     },
 
     {
@@ -115,6 +157,7 @@ const data = {
         name: 'Camiseta Atlético de Madrid',
         description: 'Camiseta del Atlético de Madrid',
         imageName: 'imageAtleticoMadrid.jpg',
+        commentsCount: 0,
     },
 
     {
@@ -122,6 +165,7 @@ const data = {
         name: 'Camiseta Paris-Saint Germain',
         description: 'Camiseta del Paris-Saint Germain',
         imageName: 'imagePSG.jpg',
+        commentsCount: 0,
     },
 
 
